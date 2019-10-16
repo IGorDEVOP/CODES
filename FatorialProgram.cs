d1924625d15f51afe098018f1291e363355ec5ee
Igor Lima | Stone <igor.lima@stone.com.br>
2019-10-16 16:07:37 -0300

    Adiciona Algoritmo do Fatorial.

---
+++ FatorialProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticandoFatorial
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+            for(int i=1; i < 8; i++)
+            {
+                fatorial *= i;
+
+
+                Console.WriteLine(" o fatorial é " + i + " = " + fatorial);
+            }
+
+
+
+
+
+            Console.WriteLine("Essa aplicação ja se encerrou. Tecle enter para sair . . ."); 
+            Console.ReadLine();
+
+        }
+    }
+}
