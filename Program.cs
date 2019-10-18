67ced0564c3aad70d1b9bca76a2163193966abf1
Igor Lima | Stone <igor.lima@stone.com.br>
2019-10-18 11:31:01 -0300

    Simplificando Multiplo de 3 em C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            
+            for (int i=1; i<=100; i++)
+            {
+                if (i %3 ==0)
+                {
+                    Console.WriteLine("Os numeros multiplos de 3 são :  " + i);
+                }
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
